7f93d7584d276a3b42d85a8ac680dad04b1ccd6f
SkullMag <rybalko.oleg.123@mail.ru>
2020-04-26 18:43:56 +0300

    Api and credentials references added

--- constants.py
+++ constants.py
@@ -1,5 +1,19 @@
 from PyQt5 import QtGui
+from rocket import RocketAPI
+import json
 
 regular_font = QtGui.QFont("fonts/Roboto-Regular.ttf")
 bold_font = QtGui.QFont("fonts/Roboto-Black.ttf")
 username_len = 9
+LINK = "https://178.76.236.166:8080"
+try:
+    credentials = json.load(open("credentials.json", encoding="utf-8"))
+except FileNotFoundError:
+    with open("credentials.json", "w", encoding="utf-8") as f:
+        f.write("{}")
+    credentials = json.load(open("credentials.json", encoding="utf-8"))
+try:
+    api = RocketAPI(credentials["login"], credentials["password"])
+except Exception as e:
+    api = RocketAPI("", "")
+    print("error")
