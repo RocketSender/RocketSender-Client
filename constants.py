61f73f211f69abc8c2577f70a1a1708944d44198
SkullMag <rybalko.oleg.123@mail.ru>
2020-04-29 19:24:13 +0300

    config variable added

--- constants.py
+++ constants.py
@@ -5,7 +5,6 @@ import json
 regular_font = QtGui.QFont("fonts/Roboto-Regular.ttf")
 bold_font = QtGui.QFont("fonts/Roboto-Black.ttf")
 username_len = 9
-LINK = "https://178.76.236.166:8080"
 try:
     credentials = json.load(open("credentials.json", encoding="utf-8"))
 except FileNotFoundError:
@@ -16,4 +15,14 @@ try:
     api = RocketAPI(credentials["login"], credentials["password"])
 except Exception as e:
     api = RocketAPI("", "")
-    print("error")
+try:
+    config = json.load(open("config.json", encoding="utf-8"))
+except FileNotFoundError:
+    with open("config.json", "w", encoding="utf-8") as f:
+        json.dump(f, {
+            "background_color": "white",
+            "text_color": "black",
+            "secondary_text_color": "gray",
+            "server_address": "https://178.76.236.166:8080"
+        })
+        config = json.load(open("config.json", encoding="utf-8"))
