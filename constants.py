5739b032768ffe5829a0b0ac8b0ee3fcd6964387
SkullMag <rybalko.oleg.123@mail.ru>
2020-05-06 08:31:08 +0300

    Contacts variable addded

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 from PyQt5 import QtGui
 from rocket import RocketAPI
+from data.contacts import Contact
 import json
+from data import db_session
+import hashlib
 
-regular_font = QtGui.QFont("fonts/Roboto-Regular.ttf")
-bold_font = QtGui.QFont("fonts/Roboto-Black.ttf")
 username_len = 9
 try:
     credentials = json.load(open("credentials.json", encoding="utf-8"))
@@ -11,6 +12,13 @@ except FileNotFoundError:
     with open("credentials.json", "w", encoding="utf-8") as f:
         f.write("{}")
     credentials = json.load(open("credentials.json", encoding="utf-8"))
+try:
+    db_session.global_init(f"db/{hashlib.sha512(credentials['login'].encode('utf-8')).hexdigest()}.db")
+    session = db_session.create_session()
+    contacts = session.query(Contact).all()
+except Exception as e:
+    session = None
+    contacts = None
 try:
     api = RocketAPI(credentials["login"], credentials["password"])
 except Exception as e:
@@ -23,6 +31,7 @@ except FileNotFoundError:
             "background_color": "white",
             "text_color": "black",
             "secondary_text_color": "gray",
-            "server_address": "https://178.76.236.166:8080"
+            "server_address": "https://178.76.236.166:8080",
+            "ipfs_api_address": "35.209.84.85:5001/api/v0"
         })
         config = json.load(open("config.json", encoding="utf-8"))
